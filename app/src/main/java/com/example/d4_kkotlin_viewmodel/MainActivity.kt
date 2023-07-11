afe7e7663bd566a6685373b1fc97ccff443d0833
jv1111 <webd0829@gmail.com>
2023-07-11 11:30:21 +0800

    updated

--- app/src/main/java/com/example/d4_kkotlin_viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/d4_kkotlin_viewmodel/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.d4_kkotlin_viewmodel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 
 // The viewModel instance, along with SavedStateHandle, helps to preserve the progress state during configuration changes like screen rotation
@@ -14,14 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
         val tv = findViewById<TextView>(R.id.tvCount)
         val btn = findViewById<Button>(R.id.button)
 
 //        tv.text = viewModel.count.toString()
-        viewModel.count.observe(this, Observer {
+        viewModel.count.observe(this) {
             tv.text = it.toString()
-        })
+        }
 
         btn.setOnClickListener{
             viewModel.updateCount()
